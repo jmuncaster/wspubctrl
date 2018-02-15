62780a9d83fc706b1a57317a8dfe06f3f9400560
Justin Muncaster <justin@muncaster.io>
2018-02-15 13:42:02 -0800

    Hide implementation

--- src/simplezmq.cpp
+++ src/simplezmq.cpp
@@ -1,28 +1,41 @@
 #include "simplezmq.hpp"
-
 #include <vector>
+#include <zmq.hpp>
 
 using namespace std;
 
 namespace simplezmq {
-  Server::Server(int pub_port, int ctrl_port) :
-    _context(),
-    _pub_socket(_context, ZMQ_PUB),
-    _reply_socket(_context, ZMQ_REP) {
+  struct Server::Detail {
+    Detail() :
+      _context(),
+      _pub_socket(_context, ZMQ_PUB),
+      _reply_socket(_context, ZMQ_REP) {
+    }
 
-    _pub_socket.bind("tcp://*:" + to_string(pub_port));
-    _reply_socket.bind("tcp://*:" + to_string(ctrl_port));
+    zmq::context_t _context;
+    zmq::socket_t _pub_socket;
+    zmq::socket_t _reply_socket;
+  };
+
+  Server::Server(int pub_port, int ctrl_port) :
+    _detail(new Detail) {
+
+    _detail->_pub_socket.bind("tcp://*:" + to_string(pub_port));
+    _detail->_reply_socket.bind("tcp://*:" + to_string(ctrl_port));
+  }
+
+  Server::~Server() { // Required for pimpl pattern
   }
 
   bool Server::check_for_request(function<string(const string&)> handler) {
-    vector<zmq::pollitem_t> items {{(void*)_reply_socket, 0, ZMQ_POLLIN, 0}};
+    vector<zmq::pollitem_t> items {{(void*)_detail->_reply_socket, 0, ZMQ_POLLIN, 0}};
     if (zmq::poll(items, 0)) {
       if (items[0].revents & ZMQ_POLLIN) {
         zmq::message_t msg;
-        _reply_socket.recv(&msg);
+        _detail->_reply_socket.recv(&msg);
         string request_payload(static_cast<char*>(msg.data()), msg.size());
         string reply_payload = handler(request_payload);
-        _reply_socket.send(reply_payload.data(), reply_payload.size());
+        _detail->_reply_socket.send(reply_payload.data(), reply_payload.size());
         return true;
       }
     }
@@ -31,34 +44,47 @@ namespace simplezmq {
   }
 
   void Server::publish(const string& payload) {
-    _pub_socket.send(payload.data(), payload.size());
+    _detail->_pub_socket.send(payload.data(), payload.size());
   }
 
+  struct Client::Detail {
+    Detail() :
+      _context(),
+      _sub_socket(_context, ZMQ_SUB),
+      _request_socket(_context, ZMQ_REQ) {
+    }
+
+    zmq::context_t _context;
+    zmq::socket_t _sub_socket;
+    zmq::socket_t _request_socket;
+  };
+
   Client::Client(const string& server_address, int sub_port, int ctrl_port) :
-    _context(),
-    _sub_socket(_context, ZMQ_SUB),
-    _request_socket(_context, ZMQ_REQ) {
+    _detail(new Detail) {
 
-    _sub_socket.setsockopt(ZMQ_SUBSCRIBE, "", 0); // Accept everything
-    _sub_socket.setsockopt(ZMQ_CONFLATE, 1);      // Drop old messages
+    _detail->_sub_socket.setsockopt(ZMQ_SUBSCRIBE, "", 0); // Accept everything
+    _detail->_sub_socket.setsockopt(ZMQ_CONFLATE, 1);      // Drop old messages
 
-    _sub_socket.connect("tcp://" + server_address + ":" + to_string(sub_port));
-    _request_socket.connect("tcp://" + server_address + ":" + to_string(ctrl_port));
+    _detail->_sub_socket.connect("tcp://" + server_address + ":" + to_string(sub_port));
+    _detail->_request_socket.connect("tcp://" + server_address + ":" + to_string(ctrl_port));
+  }
+
+  Client::~Client() { // Required for pimpl pattern
   }
 
   string Client::request(const string& payload) {
-    _request_socket.send(payload.data(), payload.size());
+    _detail->_request_socket.send(payload.data(), payload.size());
     zmq::message_t msg;
-    _request_socket.recv(&msg);
+    _detail->_request_socket.recv(&msg);
     return string(static_cast<char*>(msg.data()), msg.size());
   }
 
   string Client::wait_for_data(int timeout_ms) {
-    vector<zmq::pollitem_t> items {{(void*)_sub_socket, 0, ZMQ_POLLIN, 0}};
+    vector<zmq::pollitem_t> items {{(void*)_detail->_sub_socket, 0, ZMQ_POLLIN, 0}};
     if (zmq::poll(items, timeout_ms)) {
       if (items[0].revents & ZMQ_POLLIN) {
         zmq::message_t msg;
-        _sub_socket.recv(&msg);
+        _detail->_sub_socket.recv(&msg);
         string payload(static_cast<char*>(msg.data()), msg.size());
         return payload;
       }
--- src/simplezmq.hpp
+++ src/simplezmq.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <zmq.hpp>
-
 #include <functional>
 #include <memory>
 
@@ -13,24 +11,24 @@ namespace simplezmq {
   class Server {
     public:
       Server(int pub_port = default_data_port, int ctrl_port = default_ctrl_port);
+      ~Server();
       bool check_for_request(std::function<std::string(const std::string&)> handler);
       void publish(const std::string& payload);
 
     private:
-      zmq::context_t _context;
-      zmq::socket_t _pub_socket;
-      zmq::socket_t _reply_socket;
+      struct Detail;
+      std::unique_ptr<Detail> _detail;
   };
 
   class Client {
     public:
       Client(const std::string& server_address = "localhost", int sub_port = default_data_port, int ctrl_port = default_ctrl_port);
+      ~Client();
       std::string request(const std::string& payload);
       std::string wait_for_data(int timeout_ms = -1);
 
     private:
-      zmq::context_t _context;
-      zmq::socket_t _sub_socket;
-      zmq::socket_t _request_socket;
+      struct Detail;
+      std::unique_ptr<Detail> _detail;
   };
 }
