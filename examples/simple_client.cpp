099db518c4c2ad4a38c509b987693e045bdb545a
Justin Muncaster <justin@muncaster.io>
2018-02-16 16:31:06 -0800

    Update simple_client with some timeout handling

--- examples/simple_client.cpp
+++ examples/simple_client.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+const int timeout_ms = 5000;
+
 int main(int argc, char* argv[]) {
 
   cout << "Start client. Press ENTER to cycle through texts..." << endl;
@@ -13,26 +15,40 @@ int main(int argc, char* argv[]) {
   // Sub thread continuously reports the stream on the same line
   atomic<bool> quit(false);
   thread sub_thread([&]() {
-    zpubctrl::SubClient sub_client;
-    while (!quit) {
-      auto data = sub_client.wait_for_data();
-      cout << "\r" << data << "\e[K" << flush;
+    try {
+      zpubctrl::SubClient sub_client;
+      while (!quit) {
+        auto data = sub_client.wait_for_data(timeout_ms);
+        cout << "\r" << data << "\e[K" << flush;
+      }
+    }
+    catch (exception& e) {
+      // probably a timeout
+      cerr << e.what() << endl;
+      quit = true; // kill app on next iteration
     }
   });
 
   cin.get();
 
   // Main loop cycles through texts in response to user input
-  zpubctrl::CtrlClient ctrl_client;
-  vector<string> texts = {"Bonjour!", "Next we will try the empty string", "", "This is the last text"};
-  for (size_t i = 0; i < texts.size(); ++i) {
-    auto reply = ctrl_client.request(texts[i % texts.size()]);
-    if (reply != "OK") {
-      cout << "\n==> ERROR: " << reply << endl;
-      continue;
+  try {
+    zpubctrl::CtrlClient ctrl_client;
+    vector<string> texts = {"Bonjour!", "Next we will try the empty string", "", "This is the last text"};
+    for (size_t i = 0; !quit && i < texts.size(); ++i) {
+      auto reply = ctrl_client.request(texts[i % texts.size()], timeout_ms);
+      if (reply != "OK") {
+        cout << "\n==> ERROR: " << reply << endl;
+        continue;
+      }
+
+      cin.get(); // press enter to cycle texts
+      cout << "\e[1A" << flush; // go up a line
     }
-    cin.get(); // press enter to cycle texts
-    cout << "\e[1A" << flush; // go up a line
+  }
+  catch (exception& e) {
+    // probably a timeout
+    cerr << e.what() << endl;
   }
 
   quit = true;
