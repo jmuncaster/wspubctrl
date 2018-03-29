31318f2780ebc4fd04f2bbf386c7e902716c8da0
Justin Muncaster <justin@muncaster.io>
2018-03-29 10:23:13 -0700

    Remove zmq cmake files

--- cmake/FindZMQ.cmake
+++
@@ -1,9 +0,0 @@
-find_path(ZMQ_INCLUDE_DIR zmq.h)
-find_library(ZMQ_LIBRARY zmq)
-find_path(CPPZMQ_INCLUDE_DIR zmq.hpp)
-
-set(ZMQ_INCLUDE_DIRS ${ZMQ_INCLUDE_DIR} ${CPPZMQ_INCLUDE_DIR})
-set(ZMQ_LIBRARIES ${ZMQ_LIBRARY})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ZMQ DEFAULT_MSG ZMQ_INCLUDE_DIRS ZMQ_LIBRARIES)
