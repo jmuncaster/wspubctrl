5d5afdb2ed695871e8283018d788c9e3247c74e6
Justin Muncaster <justin@muncaster.io>
2018-12-04 12:10:19 -0800

    Remove some constants that are no longer useful b/c we want to be more explicit

--- wspubctrl/constants.hpp
+++ wspubctrl/constants.hpp
@@ -1,12 +1,8 @@
 #pragma once
 
 namespace wspubctrl {
-  constexpr const char* default_host = "localhost";
-  constexpr const char* default_pub_endpoint = "/pub";
   constexpr const char* default_ctrl_endpoint = "/ctrl";
   constexpr int default_port = 5554;
-  constexpr const char* default_pub_uri = "localhost:5554/pub";
-  constexpr const char* default_ctrl_uri = "localhost:5554/ctrl";
   constexpr int default_request_timeout_ms = 5000;
   constexpr int forever = -1;
 }
