5273176b48ca4bf6511ca02de1332cb9915e5d73
Justin Muncaster <justin@muncaster.io>
2018-12-04 14:40:06 -0700

    Simplify

--- examples/sub_client.cpp
+++ examples/sub_client.cpp
@@ -14,23 +14,28 @@ int main(int argc, char* argv[]) {
 
   string pub_uri = argv[1];
 
-  cout << "Start sub client. Use ctrl client to issue 'quit' command to shut down subscribers." << endl;
-  cout << " * subscribe: " << pub_uri << endl;
-  wspubctrl::SubClient sub_client(pub_uri);
-
-  // Continuously report the stream on the same line
   try {
+    cout << "Start sub client. Use ctrl client to issue 'quit' command to shut down subscribers." << endl;
+    cout << " * subscribe: " << pub_uri << endl;
+
+    // Continuously report the stream on the same line
+    wspubctrl::SubClient sub_client(pub_uri);
     sub_client.start();
-    string data;
-    while (data != "quit") {
-      data = sub_client.wait_for_data(timeout_ms);
-      cout << "\r" << data << "\e[K" << flush;
+    for (;;) {
+      string data = sub_client.wait_for_data(timeout_ms);
+      if (data == "quit") {
+        break;
+      }
+      else {
+        cout << "\r" << data << "\e[K" << flush;
+      }
     }
     cout << endl;
   }
   catch (exception& e) {
     // probably a timeout
     cerr << "exception: " << e.what() << endl;
+    return 1;
   }
 
   return 0;
